b0f3ce7bd193625f49fd8d8fb9d2bd44c6936fcf
Jeff-Moreau <119882736+Jeff-Moreau@users.noreply.github.com>
2023-11-17 12:21:22 -0500

    Audio done for race manager

--- Assets/Scripts/RaceManager.cs
+++ Assets/Scripts/RaceManager.cs
@@ -17,12 +17,16 @@ public class RaceManager : MonoBehaviour
     [SerializeField] private Collider _finishLine;
     [SerializeField] private VehicleMovement _player;
     [SerializeField] private LoadingManager _loadingManager;
+    [SerializeField] private AudioClip[] _countdown;
+    [SerializeField] private AudioSource _audio;
 
     private bool _gameStart = false;
     private bool _raceOver = true;
+    private bool _resetText = false;
     private float _secondsPassed = 4;
     private int _trackLaps = 2;
     private int _currentLap = 0;
+    private int _count = 0;
 
     public bool GameStarted => _gameStart;
     public int CurrentLap => _currentLap;
@@ -31,13 +35,43 @@ public class RaceManager : MonoBehaviour
     private void Update()
     {
         _secondsPassed -= Time.deltaTime;
-
+        
         if (!_gameStart && _secondsPassed > 1)
         {
+
+            if (Mathf.Floor(_secondsPassed) == 3)
+            {
+                if (!_audio.isPlaying && _count == 0)
+                {
+                    _audio.PlayOneShot(_countdown[0]);
+                    _count++;
+                }
+            }
+            else if (Mathf.Floor(_secondsPassed) == 2)
+            {
+                if (!_audio.isPlaying && _count == 1)
+                {
+                    _audio.PlayOneShot(_countdown[1]);
+                    _count++;
+                }
+            }
+            else if (Mathf.Floor(_secondsPassed) == 1)
+            {
+                if (!_audio.isPlaying && _count == 2)
+                {
+                    _audio.PlayOneShot(_countdown[2]);
+                    _count++;
+                }
+            }
             _centerText.text = Mathf.Floor(_secondsPassed).ToString();
         }
         else if (_secondsPassed < 1 && _secondsPassed >= -1)
         {
+            if (!_audio.isPlaying && _count == 3)
+            {
+                _audio.PlayOneShot(_countdown[3]);
+                _count = 0;
+            }
             _centerText.text = "GO";
             _gameStart = true;
         }
@@ -51,15 +85,27 @@ public class RaceManager : MonoBehaviour
             _lapText.text = "Lap: " + _currentLap + " of " + _trackLaps;
             _positionText.text = "Position: " + _player.CurrentPosition + " of " + (_loadingManager.RACERCOUNT + 1);
         }
-        if (_currentLap == _trackLaps)
+        if (_currentLap == _trackLaps && !_resetText)
         {
+            if (!_audio.isPlaying && _count == 0)
+            {
+                _audio.PlayOneShot(_countdown[4]);
+                _count++;
+            }
             _updateText.text = "LAST LAP!";
+            Invoke("ResetLapText", 2);
         }
         if (_currentLap > _trackLaps)
         {
             _lapText.text = "Lap: " + _trackLaps + " of " + _trackLaps;
-            _updateText.text = "YOU FINISHED " + _player.CurrentPosition;
+            _updateText.text = " YOU FINISHED " + _player.CurrentPosition;
             _raceOver = true;
         }
+    }
+
+    private void ResetLapText()
+    {
+        _resetText = true;
+        _updateText.text = "";
     }
 }
